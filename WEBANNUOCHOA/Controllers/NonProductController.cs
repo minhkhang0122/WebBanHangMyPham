f9c0f38b046129bd66a6b357b3b995d36ae2097f
kienhao2209 <kienhao2209@gmail.com>
2024-04-12 21:23:24 +0700

    ThirdCommitThayDoiLayOutCSS - em Hao

--- WEBANNUOCHOA/Controllers/NonProductController.cs
+++ WEBANNUOCHOA/Controllers/NonProductController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WEBANNUOCHOA.Repositories;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 namespace WEBANNUOCHOA.Controllers
 {
--- WEBANNUOCHOA/obj/Debug/net8.0/WEBANNUOCHOA.AssemblyInfo.cs
+++ WEBANNUOCHOA/obj/Debug/net8.0/WEBANNUOCHOA.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBANNUOCHOA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb8f2ebd4811ade87c29178ab1c2697e32904e03")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBANNUOCHOA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBANNUOCHOA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
